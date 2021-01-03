76f725991bf45dd073ee705e5778ec1b6db376a3
mateenkazia <mateen.kazia@chaione.com>
2021-01-02 19:25:03 -0600

    setting up files

---
+++ sesson7/index.js
@@ -0,0 +1,18 @@
+// create map function
+
+const myMap = (arr, func) => {
+
+};
+
+// console.log(myMap([1, 2, 3, 4, 5], (item) => item + 1));
+
+// create filter function
+const myFilter = (arr, func) => {
+
+};
+
+console.log(myFilter([1, 2, 3, 4, 5], (item) => item % 2));
+
+// create reducer function
+
+const reducer = () => {};
